3ad776743bf0d2634b4834ee5e9ccc6998d30d35
zeroflag <m.magyar3@gmail.com>
2025-01-03 19:32:47 +0100

    new style macros work in prog

---
+++ ast.lua
@@ -0,0 +1,207 @@
+local ast = {}
+
+function ast.func_definition(func_name, children)
+  return {
+    name = "function",
+    func_name = name,
+    children = children
+  }
+end
+
+function ast.pop()
+  return {name = "stack_op", op  = "pop"}
+end
+
+function ast.pop2nd()
+  return {name = "stack_op", op  = "pop2nd"}
+end
+
+function ast.pop3rd()
+  return {name = "stack_op", op  = "pop3rd"}
+end
+
+function ast.stack_op(operation)
+  return {
+    name = "stack_op",
+    op = operation
+  }
+end
+
+function ast.push(item)
+  return {
+    name  = "push",
+    children = { item }
+  }
+end
+
+function ast.aux_push(item)
+  return {
+    name  = "push-aux",
+    children = { item }
+  }
+end
+
+function ast.aux_op(operation)
+  return {
+    name = "aux_op",
+    subtype = operation
+  }
+end
+
+function ast.to_aux(item)
+  return {
+    name = "to_aux",
+    children = { item }
+  }
+end
+
+function ast.from_aux()
+  return {
+    name = "from_aux"
+  }
+end
+
+function ast._while(conditional)
+  return {
+    name = "while",
+    conditional = conditional
+  }
+end
+
+function ast._until(conditional)
+  return {
+    name = "until",
+    conditional = conditional
+  }
+end
+
+function ast.literal(subtype, value)
+  return {
+    name = "literal",
+    subtype = subtype,
+    value = value,
+  }
+end
+
+function ast.bin_op(operator, param1, param2, use_locals)
+  return {
+    name = "bin_op",
+    op = operator,
+    p1 = param1,
+    p2 = param2,
+    use_locals = use_locals
+  }
+end
+
+function ast.unary_op(operator, operand)
+  return {
+    name = "unary_op",
+    op = operator,
+    p1 = operand
+  }
+end
+
+function ast.nullary_operator(operator, operand)
+  return {
+    name = "nullary_operator",
+    subtype = operator
+  }
+end
+
+function ast.assignment(var, exp)
+  return {
+    name = "assignment",
+    var  = var,
+    exp  = exp
+  }
+end
+
+function ast.def_local(variable)
+  return {
+    name = "local",
+    var = variable
+  }
+end
+
+function ast.var_ref(variable)
+  return {
+    name = "varref",
+    var_name = variable
+  }
+end
+
+function ast.new_table()
+  return {
+    name = "table_new"
+  }
+end
+
+function ast.table_at(key, tbl)
+  return {
+    name = "table_at",
+    children = { tbl, key }
+  }
+end
+
+function ast.table_put(value, key, tbl)
+  return {
+    name = "table_put",
+    children = { tbl, key, value }
+  }
+end
+
+function ast._if(cond)
+  return {
+    name = "if",
+    cond = cond
+  }
+end
+
+function ast._else()
+  return {name = "else"}
+end
+
+function ast._end()
+  return {name = "end"}
+end
+
+function ast._break()
+  return {name = "break"}
+end
+
+function ast._repeat()
+  return {name = "repeat"}
+end
+
+function ast._return()
+  return {name = "return"}
+end
+
+function ast._for(loop_var, start, stop, step)
+  return {
+    name = "for",
+    loop_var = loop_var,
+    start = start,
+    stop = stop,
+    step = step
+  }
+end
+
+function ast._foreach(loop_var1, loop_var2, iterable)
+  return {
+    name = "for_each",
+    loop_var1 = loop_var1,
+    loop_var2 = loop_var2,
+    iterable = iterable
+  }
+end
+
+function ast._ipairs(iterable)
+  return {name = "ipairs", iterable = iterable}
+end
+
+function ast._pairs(iterable)
+  return {name = "pairs", iterable = iterable}
+end
+
+return ast
