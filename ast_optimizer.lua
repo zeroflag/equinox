88f89a2a27706815962df765fc35b682665126d4
zeroflag <m.magyar3@gmail.com>
2025-01-04 14:29:53 +0100

    refac

--- ast_optimizer.lua
+++ ast_optimizer.lua
@@ -6,16 +6,6 @@ function Optimizer.new()
   return obj
 end
 
---[[
-# 1 5 to: i i i * . end
-push: {"item":{"kind":"number","name":"literal","value":1},"name":"push"}
-push: {"item":{"kind":"number","name":"literal","value":5},"name":"push"}
-push: {"item":{"id":"i","name":"identifier"},"name":"push"}
-push: {"item":{"id":"i","name":"identifier"},"name":"push"}
-push: {"item":{"name":"bin_op","op":"*","p1":{"name":"stack_op","op":"pop"},"p2":{"name":"stack_op","op":"pop"}},"name":"push"}
-1 4 9 16 25 OK
-]]
-
 function is(ast, name)
   return ast.name == name
 end
@@ -40,10 +30,15 @@ function is_unop(ast)
   return is(ast, "push") and is(ast.item, "unary_op")
 end
 
+function is_tbl_at(ast)
+  return is(ast, "table_at") -- no push here
+end
+
 function is_assignment(ast)
   return is(ast, "assignment")
 end
 
+local tbl_at_inline_params = {is_lit_or_id, is_lit_or_id, is_tbl_at}
 local binop_inline_params = {is_lit_or_id, is_lit_or_id, is_binop}
 local unop_inline_param = {is_lit_or_id, is_unop}
 local assignment_inline_param = {is_lit_or_id, is_assignment}
@@ -62,17 +57,27 @@ function Optimizer:optimize_ast(ast)
   local i = 1
   while i <= #ast do
     local node = ast[i]
-    if match(binop_inline_params, ast, i) then
-      local p1, p2, op = ast[i],  ast[i + 1], ast[i + 2]
+    -- tbl const/var at
+    if match(tbl_at_inline_params, ast, i) then
+      local tbl, idx, op = ast[i], ast[i + 1], ast[i + 2]
+      op.tbl = tbl.item
+      op.key = idx.item
+      table.insert(result, op)
+      i = i + #tbl_at_inline_params
+    -- const/var const/var OP
+    elseif match(binop_inline_params, ast, i) then
+      local p1, p2, op = ast[i], ast[i + 1], ast[i + 2]
       op.item.p1 = p1.item
       op.item.p2 = p2.item
       table.insert(result, op)
       i = i + #binop_inline_params
+    -- const/var OP
     elseif match(unop_inline_param, ast, i) then
       local p1, op = ast[i], ast[i + 1]
       op.item.p1 = p1.item
       table.insert(result, op)
       i = i + #unop_inline_param
+    -- const/var -> VAR
     elseif match(assignment_inline_param, ast, i) then
       local p1, op = ast[i], ast[i + 1]
       op.exp = p1.item
