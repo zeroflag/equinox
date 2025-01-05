963de42e46e4d471af54705bb91f1e56d52adf29
zeroflag <m.magyar3@gmail.com>
2025-01-06 00:01:03 +0100

    matcher comments

--- ast_matchers.lua
+++ ast_matchers.lua
@@ -163,6 +163,13 @@ function PutParamsInline:optimize(ast, i, result)
   table.insert(result, op)
 end
 
+--[[
+ Inline IF conditional
+
+  1.) false not IF ... THEN   =>   IF not false THEN ... END
+  2.) 10 v  <   IF ... THEN   =>   IF 10 < v    THEN ... END
+  3.)    v      IF ... THEN   =>   IF v         THEN ... END
+]]--
 function IfCondInline:optimize(ast, i, result)
   self:log("inlining if condition")
   local cond, _if = ast[i], ast[i + 1]
@@ -170,6 +177,11 @@ function IfCondInline:optimize(ast, i, result)
   table.insert(result, _if)
 end
 
+--[[
+ Inline assignment operator's value operand
+
+  1.) 123 -> v   =>   v = 123
+]]--
 function AssignmentInline:optimize(ast, i, result)
   self:log("inlining assignment operator's param")
   local p1, op = ast[i], ast[i + 1]
@@ -177,6 +189,11 @@ function AssignmentInline:optimize(ast, i, result)
   table.insert(result, op)
 end
 
+--[[
+ Inline unary operator's constant param
+
+  1.) false not   =>   PUSH(not false)
+]]--
 function UnaryInline:optimize(ast, i, result)
   self:log("inlining unary operator's param")
   local p1, op = ast[i], ast[i + 1]
@@ -184,6 +201,11 @@ function UnaryInline:optimize(ast, i, result)
   table.insert(result, op)
 end
 
+--[[
+ Inline DUP followed by unary operator
+
+  1.) [ 1 2 3 ] DUP size   =>   PUSH(#TOS)
+]]--
 function DupUnaryInline:optimize(ast, i, result)
   self:log("inlining dup before unary operator")
   local p1, op = ast[i], ast[i + 1]
@@ -192,14 +214,25 @@ function DupUnaryInline:optimize(ast, i, result)
   table.insert(result, op)
 end
 
+--[[
+ Inline DUP followed by binary operator
+
+  1.) 3 DUP *   =>   PUSH(TOS * POP)
+]]--
 function DupBinaryInline:optimize(ast, i, result)
   self:log("inlining dup before binary operator")
-  local p1, op = ast[i], ast[i + 1]
+  local p1, p2, op = ast[i], ast[i + 1], ast[i + 2]
   op.item.p1.op = "tos"
   op.item.p1.name = "stack_op" -- replace stack_access to stack_os, to prevent further inlining
+  table.insert(result, p1)
   table.insert(result, op)
 end
 
+--[[
+ Inline DUPs followed by binary operator
+
+  1.) DUP DUP *   =>   PUSH(TOS * TOS)
+]]--
 function DupDupBinaryInline:optimize(ast, i, result)
   self:log("inlining dup dup before binary operator")
   local p1, p2, op = ast[i], ast[i + 1], ast[i + 2]
@@ -210,6 +243,11 @@ function DupDupBinaryInline:optimize(ast, i, result)
   table.insert(result, op)
 end
 
+--[[
+ Inline DUP followed by IF
+
+  1.) DUP IF .. THEN   =>   TOS IF .. THEN
+]]--
 function DupIfInline:optimize(ast, i, result)
   self:log("inlining dup before if")
   local dup, _if = ast[i], ast[i + 1]
@@ -218,6 +256,13 @@ function DupIfInline:optimize(ast, i, result)
   table.insert(result, _if)
 end
 
+--[[
+ Inline binary operator's ALL constant operands
+
+  1.) 12      45 +   =>   PUSH(12   + 45)
+  2.) 12      v1 +   =>   PUSH(12   + v1)
+  3.) t 1 at  45 +   =>   PUSH(t[1] + 45)
+]]--
 function BinaryInline:optimize(ast, i, result)
   self:log("inlining binary operator params")
   local p1, p2, op = ast[i], ast[i + 1], ast[i + 2]
@@ -226,15 +271,28 @@ function BinaryInline:optimize(ast, i, result)
   table.insert(result, op)
 end
 
+--[[
+ Inline binary operator's 2nd, constant operand
+ Additonally replace DUP with DROP if applicable
+
+  1.)      123 +   =>   PUSH(POP + 123)
+  2.)  DUP 123 +   =>   PUSH(TOS + 123)
+]]--
 function BinaryInlineP2:optimize(ast, i, result)
   self:log("inlining binary operator's 2nd param")
   local p1, p2, op = ast[i], ast[i + 1], ast[i + 2]
-  if op.item.p1.name == "stack_access" and
-     op.item.p1.op == "pop2nd" then
-    op.item.p1.op = "pop"
+  if op.item.p1.name == "stack_access" then
+    if op.item.p1.op == "pop2nd" then
+      op.item.p1.op = "pop"
+    end
+    if is_stack_op("dup")(p1) then
+      op.item.p1.op = "tos" -- inline if dup
+    end
   end
-  op.item.p2 = p2.item
-  table.insert(result, p1)
+  op.item.p2 = p2.item -- inline const param
+  if not is_stack_op("dup")(p1) then -- dup was inlined skip it
+    table.insert(result, p1)
+  end
   table.insert(result, op)
 end
 
@@ -246,6 +304,7 @@ function InlineInitLocalConst:optimize(ast, i, result)
 end
 
 return {
+
   PutParamsInline:new(
     "inline put params ",
     {is_push_const, is_push_const, is_push_const, is_tbl_put}),
@@ -275,15 +334,15 @@ return {
     {is_stack_op("dup"), is_push_unop_pop}),
 
   DupDupBinaryInline:new(
-    "dup unary inline",
+    "dup dup binary inline",
     {is_stack_op("dup"), is_stack_op("dup"), is_push_binop_pop}),
 
   DupBinaryInline:new(
-    "dup unary inline",
-    {is_stack_op("dup"), is_push_binop_pop}),
+    "dup binary inline",
+    {NOT(is_stack_op("dup")), is_stack_op("dup"), is_push_binop_pop}),
 
   DupIfInline:new(
-    "dup unary inline",
+    "dup if inline",
     {is_stack_op("dup"), is_if}),
 
   AssignmentInline:new(
@@ -294,7 +353,7 @@ return {
     "if cond inline",
     {OR(is_push_const,
         is_push_unop,
-        is_push_binop), is_if}),
+        is_push_binop), is_if}), -- TODO 10 3 over < if
 
   InlineInitLocalConst:new(
     "inline init local const", -- only optimizes one parameter
