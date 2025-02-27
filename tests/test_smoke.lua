325e92f545a22a58b74367809c3dfe347f80abf8
zeroflag <m.magyar3@gmail.com>
2025-02-27 14:46:32 +0100

    smoke test

--- tests/test_smoke.lua
+++ tests/test_smoke.lua
@@ -3,5 +3,4 @@ local command = 'echo "7 3 * dup + .\nbye" | make repl | grep "OK"'
 local handle = io.popen(command)
 local result = handle:read("*a")
 handle:close()
-print(result)
--- assert(result:match("^42.*")) TODO
+assert(result:match("%D*42%D*"))
