c411abf36b78c62dd51b3b90bc9116e168c56823
zeroflag <m.magyar3@gmail.com>
2025-02-27 14:43:38 +0100

    debug ci problem

--- tests/test_smoke.lua
+++ tests/test_smoke.lua
@@ -3,4 +3,5 @@ local command = 'echo "7 3 * dup + .\nbye" | make repl | grep "OK"'
 local handle = io.popen(command)
 local result = handle:read("*a")
 handle:close()
+print(result)
 -- assert(result:match("^42.*")) TODO
